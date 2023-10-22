d427758cbf5954b5569cb3005b095ae6a3a059b7
Ghaith Othmani <none>
2023-10-22 08:27:25 +0100

    added and deleted a component to test it out (not important)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AsdComponent } from './asd/asd.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AsdComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
