217cb22936eff80dfbadd8fa3baef614a3b85a3b
Ghaith Othmani <none>
2023-10-22 08:54:37 +0100

    created "ListEvent" Component and organized the workspace

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EventComponent } from './event/event.component';
+import { EventComponent } from './Etudiant/event/event.component';
+import { ListEventComponent } from './Etudiant/list-event/list-event.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EventComponent
+    EventComponent,
+    ListEventComponent
   ],
   imports: [
     BrowserModule,
