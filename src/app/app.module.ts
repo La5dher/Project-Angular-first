0d65f0510b8c04a469c5681a724af378bdefc13c
Ghaith Othmani <none>
2023-10-22 08:06:24 +0100

    a new version without the default Angular template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AsdComponent } from './asd/asd.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AsdComponent
   ],
   imports: [
     BrowserModule,
