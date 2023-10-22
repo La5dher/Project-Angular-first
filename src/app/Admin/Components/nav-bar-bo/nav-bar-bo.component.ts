afb1ff53ef89e8796cde4e46343c4d1a380860b5
Ghaith Othmani <none>
2023-10-22 17:27:07 +0100

    Main menu base form(not much CSS) - added a main menu allowing to choose between interfaces (BO/FO) - added a link allowing to get back to the main menu from the home of each interface - added the children routes to be able to link between the BO and FO

---
+++ src/app/Admin/Components/nav-bar-bo/nav-bar-bo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar-bo',
+  templateUrl: './nav-bar-bo.component.html',
+  styleUrls: ['./nav-bar-bo.component.css']
+})
+export class NavBarBOComponent {
+
+}
---
+++ src/app/Etudiant/main-fo/main-fo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-fo',
+  templateUrl: './main-fo.component.html',
+  styleUrls: ['./main-fo.component.css']
+})
+export class MainFoComponent {
+
+}
